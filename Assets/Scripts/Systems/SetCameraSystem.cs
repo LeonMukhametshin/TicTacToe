727f131876772f27c45639494a00c6f3b2e9b317
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2026-02-07 14:19:22 +0300

    Change camera position

--- Assets/Scripts/Systems/SetCameraSystem.cs
+++ Assets/Scripts/Systems/SetCameraSystem.cs
@@ -19,14 +19,19 @@ namespace TicToe.Systems
             }
 
             var height = m_configuration.levelHeight;
+            var width = m_configuration.levelWidth;
+            var boardHeight = height + (height - 1) * m_configuration.offset.y;
+            var boardWidth = width + (width - 1) * m_configuration.offset.x;
+            var hudSpace = Mathf.Max(1f, boardHeight * 0.2f);
+            var totalHeight = boardHeight + hudSpace;
 
             var camera = m_sceneData.Camera;
             camera.orthographic = true;
-            camera.orthographicSize = height / 2f + (height - 1) * m_configuration.offset.y / 2;
+            camera.orthographicSize = totalHeight / 2f;
 
             m_sceneData.cameraTransfrom.position = new Vector3(
-                m_configuration.levelWidth / 2f + (m_configuration.levelWidth - 1) * m_configuration.offset.x / 2, 
-                height / 2f + (height - 1) * m_configuration.offset.y / 2);
+                boardWidth / 2f,
+                boardHeight / 2f + hudSpace / 2f);
         }
     }
 }
